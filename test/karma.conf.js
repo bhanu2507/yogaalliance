840e010a22d44f7818149d4bfc8136d2e6dcafcf
bmokkala <sai2903!>
2016-06-30 18:34:33 +0530

    styling update 1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,6 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/bootstrap-css/js/bootstrap.min.js',
+      'bower_components/responsive-nav/responsive-nav.js',
       'bower_components/mockfirebase/browser/mockfirebase.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
